6cf090067d93280acf99f9178f6c6f3c802804fc
Christopher Piscopio <chris@coinsource.net>
2022-08-31 13:08:34 -0500

    added static component header

--- shell/src/app/header/header.component.ts
+++ shell/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild('headerRef', {read: ViewContainerRef})
+  viewContainer!: ViewContainerRef
+
   constructor() { }
 
   ngOnInit(): void {
+    this.load();
+  }
+
+  async load(): Promise<void> {
+    const module = await loadRemoteModule({
+      type: 'module',
+      remoteEntry: 'http://localhost:4201/remoteEntry.js',
+      exposedModule: './HeaderComponent'
+    });
+
+    const ref = this.viewContainer.createComponent(module.HeaderCompComponent);
   }
 
 }
